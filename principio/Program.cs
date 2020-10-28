fdd59b21395e003f5c6408ee635031fe05b913c5
eeyunn <73603066+eeyunn@users.noreply.github.com>
2020-10-28 21:14:39 +0100

    Update Program.cs

--- principio/Program.cs
+++ principio/Program.cs
@@ -7,6 +7,13 @@ namespace principio
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string prr;
+            int a;
+
+            prr = Console.ReadLine();
+            a = int.Parse(prr);
+            Console.WriteLine(a * 4);
+            Console.ReadKey();
         }
     }
 }
