41cd1b2ae25a525ff979ff658ac2308a3a96bab9
eeyunn <73603066+eeyunn@users.noreply.github.com>
2020-10-29 11:54:04 +0100

    ddd

--- principio/Program.cs
+++ principio/Program.cs
@@ -6,10 +6,9 @@ namespace principio
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             string prr;
             int a;
-
+            Console.WriteLine("Dame un numerito.");
             prr = Console.ReadLine();
             a = int.Parse(prr);
             Console.WriteLine(a * 4);
