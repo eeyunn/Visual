477c6a11bde18ad5bbf99ecdda412f1823a01c7e
eeyunn <73603066+eeyunn@users.noreply.github.com>
2020-10-29 12:16:34 +0100

    Ejemplo2

---
+++ Condicional/Condicional/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Condicional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sueldo;
+            string linea;
+            Console.Write("Introduce el sueldo: ");
+            linea = Console.ReadLine();
+            sueldo = double.Parse(linea);
+            if (sueldo > 3000)
+            {
+                Console.WriteLine("Tenés que pagar impuestos.");
+            }
+        }
+    }
+}
