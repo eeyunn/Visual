d09419c042c7a59abea8c7fcd7cfdf03ffed9b27
eeyunn <73603066+eeyunn@users.noreply.github.com>
2020-10-29 12:31:42 +0100

    ej7

---
+++ Condicional2/Condicional2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Condicional2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Escribe un número.");
+            string paco = Console.ReadLine();
+            num = int.Parse(paco);
+
+            if((num > 0) && (num < 100))
+            {
+            if ((num / 10) >= 1) Console.WriteLine("Dos dígitos.");
+            else Console.WriteLine("Un dígito.");
+            }
+            
+
+        }
+    }
+}
